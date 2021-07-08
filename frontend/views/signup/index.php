7adccbb39b4d815fdf942fee3e66ff2ee73af7bc
Boris Shkarov <b.shkarov@gmail.com>
2021-07-08 15:32:53 +0300

    module6-task3.Исправления

--- frontend/views/signup/index.php
+++ frontend/views/signup/index.php
@@ -4,6 +4,7 @@
 ?>
 
 <section class="registration__user">
+
   <h1>Регистрация аккаунта</h1>
   <div class="registration-wrapper">
 
@@ -65,6 +66,18 @@
 
     <?php ActiveForm::end(); ?>
 
+  </div>
+  <div class="clipart-woman">
+    <img src="./img/clipart-woman.png" width="238" height="450">
+  </div>
+  <div class="clipart-message">
+    <div class="clipart-message-text">
+      <h2>Знаете ли вы, что?</h2>
+      <p>После регистрации вам будет доступно более
+        двух тысяч заданий из двадцати разных категорий.</p>
+      <p>В среднем, наши исполнители зарабатывают
+        от 500 рублей в час.</p>
+    </div>
   </div>
 
 </section>
